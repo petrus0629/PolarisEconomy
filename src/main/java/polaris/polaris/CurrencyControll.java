653983134cd8025b00563454f1fccc6f858dfbd1
petrus0629 <59354462+petrus0629@users.noreply.github.com>
2023-01-17 04:13:39 +0900

    third

--- src/main/java/polaris/polaris/CurrencyControll.java
+++ src/main/java/polaris/polaris/CurrencyControll.java
@@ -1,5 +1,6 @@
 package polaris.polaris;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,7 +15,20 @@ public class CurrencyControll implements CommandExecutor {
         {
             Player player = (Player) sender;
 
-            if(command.getName().equalsIgnoreCase("돈"))
+            if(command.getName().equalsIgnoreCase("은행"))
+            {
+                if(args[0].equalsIgnoreCase("잔고"))
+                {
+                    if(args.length == 3)
+                    {
+                        try{
+                            Player tartget = Bukkit.getPlayer(args[1]);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
         }
         return true;
     }
